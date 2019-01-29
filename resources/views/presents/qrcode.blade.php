aef231fcc80349e563c678a041e02b44de6301a3
Pthamonwann <pthamonwann@hotmail.com>
2019-01-29 20:58:35 +0700

    Change code in qr to url/code

--- resources/views/presents/qrcode.blade.php
+++ resources/views/presents/qrcode.blade.php
@@ -12,7 +12,7 @@
                 <div class="row text-center" style="padding-left: 20px">
                 <h1>Present {{ $present->sequence->description}}</h1>
                 <h2>Room {{ $present->room->name}}</h2>
-                    <img src="https://chart.googleapis.com/chart?chs=450x450&cht=qr&chl={{ $present->code }}">
+                    <img src="https://chart.googleapis.com/chart?chs=450x450&cht=qr&chl=http://35.247.147.98:8084/qrcode/scan/{{ $present->code }}">
                     {{-- <a href="{!! route('presents.index') !!}" class="btn btn-default">Back</a> --}}
                 </div>
             </div>
