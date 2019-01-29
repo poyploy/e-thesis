67a026da562f9e128e07ce7a85c829b727b63512
Pthamonwann <pthamonwann@hotmail.com>
2019-01-29 20:38:45 +0700

    navigator.getUserMedia

--- resources/views/qrcode/qrcode.blade.php
+++ resources/views/qrcode/qrcode.blade.php
@@ -98,7 +98,7 @@
     canvas.stroke();
   }
   // Use facingMode: environment to attemt to get the front camera on phones
-  navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function (stream) {
+  navigator.getUserMedia({ video: { facingMode: "environment" } }).then(function (stream) {
     video.srcObject = stream;
     video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
     video.play();
