220ebcd7fda7a441e8baac81827ec2dfb9153325
JIrayut Khantavee <ker.koleng@hotmail.co.th>
2019-02-14 22:41:31 +0700

    Add new table for remove user in room

--- resources/views/rooms/manual.blade.php
+++ resources/views/rooms/manual.blade.php
@@ -22,5 +22,39 @@
             </div>
         </div>
     </div>
+
+    <div class="box box-primary">
+        <div class="box-body">
+            <h3>Student in room</h3>
+            <div class="row">
+                <table class="table table-responsive" id="users-room-table">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Year</th>
+                            <th>#<th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($userRooms as $item)
+                        <tr>
+                            <td>{!! $item->user->id !!}</td>
+                            <td>{!! $item->user->name_TH !!}</td>
+                            <td>{!! $item->user->email !!}</td>
+                            <td>{!! $item->user->year !!}</td>
+                            <td>
+                                {!! Form::open(['route' => ['rooms.destroy.user', $room->id, $item->user->id], 'method' => 'delete']) !!} 
+                                {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                                {!! Form::close() !!} 
+                            </td>        
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
